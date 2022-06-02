ea6b29235809f17d6811cdbb180b7ed5779c196f
issahnevan <issahnevan@gmail.com>
2022-06-03 00:07:40 +0800

    a

--- verification.php
+++ verification.php
@@ -1,42 +1,93 @@
-<?php
- 
-    if (isset($_POST["verify_email"]))
-    {
-        $email = $_POST["email"];
-        $verification_code = $_POST["verification_code"];
- 
-        // connect with database
-        $conn = mysqli_connect("localhost:3306", "root", "", "test");
- 
-        // mark email as verified
-        $sql = "UPDATE users SET email_verified_at = NOW() WHERE email = '" . $email . "' AND verification_code = '" . $verification_code . "'";
-        $result  = mysqli_query($conn, $sql);
- 
-        if (mysqli_affected_rows($conn) == 0)
-        {
-            die("Verification code failed.");
-        }
- 
-        echo "<p>You can login now.</p>";
-        exit();
-    }
- 
-?>
+<?php session_start() ?>
 
-<!DOCTYPE html>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Email-Verification</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="icon" href="Favicon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <title>Verification</title>
 </head>
 <body>
-<form method="POST">
-    <input type="hidden" name="email" value="<?php echo $_POST["email"];?>">
-    <input type="text" name="verification_code" placeholder="Enter verification code" required />
- 
-    <input type="submit" name="verify_email" value="Verify Email">
-</form>
+
+<nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
+    <div class="container">
+        <a class="navbar-brand" href="#">Verification Account</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+</nav>
+
+<main class="login-form">
+    <div class="cotainer">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Verification Account</div>
+                    <div class="card-body">
+                        <form action="#" method="POST">
+                            <div class="form-group row">
+                                <label for="email_address" class="col-md-4 col-form-label text-md-right">OTP Code</label>
+                                <div class="col-md-6">
+                                    <input type="text" id="otp" class="form-control" name="otp_code" required autofocus>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6 offset-md-4">
+                                <input type="submit" value="Verify" name="verify">
+                            </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+</main>
 </body>
 </html>
+<?php 
+    include('connect/connection.php');
+    if(isset($_POST["verify"])){
+        $otp = $_SESSION['otp'];
+        $email = $_SESSION['mail'];
+        $otp_code = $_POST['otp_code'];
+
+        if($otp != $otp_code){
+            ?>
+           <script>
+               alert("Invalid OTP code");
+           </script>
+           <?php
+        }else{
+            mysqli_query($connect, "UPDATE login SET status = 1 WHERE email = '$email'");
+            ?>
+             <script>
+                 alert("Verfiy account done, you may sign in now");
+                   window.location.replace("index.php");
+             </script>
+             <?php
+        }
+
+    }
+
+?>
