c6e2b6f29abde3c8415d3f4c10eb1980517c7651
issahnevan <issahnevan@gmail.com>
2022-06-06 02:56:53 +0800

    s

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
-    <!-- Nav bar section -->
-    <div class="header">
-            <?php
-            if(isset($_SESSION['SessionName'])){
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+<!-- Nav bar section -->
+<div class="header">
+<?php
+    if(isset($_SESSION[''])){
+        header("location:.php");
+    } else{
+        include_once("includes/Header.php");
+    }
+?>
+</div>
 
-                header("location:Freelancer_Dashboard.php");
-                
-            }
-            else{
-                include_once("includes/Header.php");
-            }
-            ?>
-        </div>
+
+
+</body>
+</html>
--- login.php
+++ login.php
@@ -10,15 +10,12 @@
 </head>
 <body>
 <div class="login-container">
-        
     <div class="login-input">
-
         <form action="index.php" method="post">
                 <div class="login-title">KAPADYAK</div>
                 <div><label class="">LOG IN</label></div>
                 <div><label class="">EMAIL</label></div>
                 <div><input type="text" name="email" class=""></div>
-          
                 <div><label class="">PASSWORD</label></div>
                 <div><input type="password" name="password" class=""></div>
                 <div><input type="submit" name="login" value="LOG IN" class=""></div>
@@ -28,11 +25,10 @@
     <div class="login-logo">
         <img src="Images/logo.png">
     </div>
-            
-
     <div class="footer">
         <div class="login-footer">&#169; 2022 Kapadyak, All Rights Reserved.</div>
     </div> 
 </div>
+
 </body>
 </html>
