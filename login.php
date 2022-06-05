13fead2bf75cd916a2d162469460047b71d1dd24
Astozel <97175301+Astozel@users.noreply.github.com>
2022-06-05 23:33:55 +0800

    Front Color

--- login.php
+++ login.php
@@ -13,8 +13,9 @@
         
     <div class="login-input">
         <form action="Backend/Login.php" method="post">
-                <div><label class="">LOG IN</label></div>
-                <div><label class="">EMAIL</label></div>
+                <div class="login-title">KAPADYAK</div>
+                <div>LOG IN</label></div>
+                <div>EMAIL</label></div>
                 <div><input type="text" name="emailUser" class=""></div>
                 <div><label class="">PASSWORD</label></div>
                 <div><input type="password" name="Password_TB" class=""></div>
@@ -22,14 +23,14 @@
         </form>
         <div><a href="user_register.php">Create an Account</a></div>
     </div>
-
     <div class="login-logo">
         <img src="Images/logo.png">
     </div>
             
+
     <div class="footer">
-    <?php include_once('Includes/footer.php')?>
-    </div>
+        <div class="login-footer">&#169; 2022 Kapadyak, All Rights Reserved.</div>
+    </div> 
 </div>
 </body>
 </html>
