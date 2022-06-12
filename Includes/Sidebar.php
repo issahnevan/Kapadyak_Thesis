8dafc62d55fb86b3711ab0aa253259b33f02e4c9
Astozel <97175301+Astozel@users.noreply.github.com>
2022-06-12 22:15:15 +0800

    Sidebar

---
+++ Includes/Sidebar.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap"
+        rel="stylesheet"
+    />
+    <title>Sidebar</title>
+</head>
+<body>
+    <nav class="navbar">
+        <ul class="navbar-nav">
+<!-- Newsfeed -->
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi bi-house-heart" viewBox="0 0 16 16">
+                <g class="fa-group">
+                    <path
+                        fill="currentColor"
+                        d="M8 6.982C9.664 5.309 13.825 8.236 8 12 2.175 8.236 6.336 5.309 8 6.982Z"
+                        class="fa-secondary"
+                    ></path>
+                    <path
+                        fill="currentColor"
+                        d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.707L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.646a.5.5 0 0 0 .708-.707L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"
+                        class="fa-primary"
+                    ></path>
+                </g>
+                </svg>
+                <span class="link-text">Newsfeed</span>
+                </a>
+            </li>
+<!-- Profile -->
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                <g class="fa-group">
+                    <path
+                        fill="currentColor"
+                        d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"
+                        class="fa-secondary"
+                    ></path>
+                    <path
+                        fill="currentColor"
+                        fill-rule="evenodd" 
+                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                        class="fa-primary"
+                    ></path>
+                </g>
+                </svg>
+                <span class="link-text">Profile</span>
+                </a>
+            </li>
+<!-- Message -->
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-chat-dots" viewBox="0 0 16 16">
+                <g class="fa-group">
+                    <path
+                        fill="currentColor"
+                        d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"
+                        class="fa-secondary"
+                    ></path>
+                    <path
+                        fill="currentColor"
+                        d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2z"
+                        class="fa-primary"
+                    ></path>
+                </g>
+                </svg>
+                <span class="link-text">Message</span>
+                </a>
+            </li>
+<!-- Rental -->
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi bi-bicycle" viewBox="0 0 16 16">
+                <path />
+                <g class="fa-group">
+                    <path
+                        fill="currentColor"
+                        d="M4 4.5a.5.5 0 0 1 .5-.5H6a.5.5 0 0 1 0 1v.5h4.14l.386-1.158A.5.5 0 0 1 11 4h1a.5.5 0 0 1 0 1h-.64l-.311.935.807 1.29a3 3 0 1 1-.848.53l-.508-.812-2.076 3.322A.5.5 0 0 1 8 10.5H5.959a3 3 0 1 1-1.815-3.274L5 5.856V5h-.5a.5.5 0 0 1-.5-.5zm1.5 2.443-.508.814c.5.444.85 1.054.967 1.743h1.139L5.5 6.943zM8 9.057 9.598 6.5H6.402L8 9.057zM4.937 9.5a1.997 1.997 0 0 0-.487-.877l-.548.877h1.035zM3.603 8.092A2 2 0 1 0 4.937 10.5H3a.5.5 0 0 1-.424-.765l1.027-1.643zm7.947.53a2 2 0 1 0 .848-.53l1.026 1.643a.5.5 0 1 1-.848.53L11.55 8.623z"
+                        class="fa-primary"
+                    ></path>
+                </g>
+                </svg>
+                <span class="link-text">Rental</span>
+                </a>
+            </li>
+<!-- Pre-loved -->
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi bi-bag-heart" viewBox="0 0 16 16">
+                <path  />
+                <g class="fa-group">
+                    <path
+                        fill="currentColor"
+                        fill-rule="evenodd"
+                        d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5Zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0ZM14 14V5H2v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1ZM8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"
+                        class="fa-primary"
+                    ></path>
+                </g>
+                </svg>
+                <span class="link-text">Pre-loved</span>
+                </a>
+            </li>
+<!-- Event -->
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi bi-calendar-event" viewBox="0 0 16 16">
+                <g class="fa-group">
+                    <path
+                        fill="currentColor"
+                        d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"
+                        class="fa-secondary"
+                    ></path>
+                    <path
+                        fill="currentColor"
+                        d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"
+                        class="fa-primary"
+                    ></path>
+                </g>
+                </svg>
+                <span class="link-text">Event</span>
+                </a>
+            </li>
+<!-- Gallery -->
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi bi-images" viewBox="0 0 16 16">
+                <path />
+                <path />
+                <g class="fa-group">
+                    <path
+                        fill="currentColor"
+                        d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"
+                        class="fa-secondary"
+                    ></path>
+                    <path
+                        fill="currentColor"
+                        d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2zM14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1zM2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1h-10z"
+                        class="fa-primary"
+                    ></path>
+                </g>
+                </svg>
+                <span class="link-text">Gallery</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <main>
+        <h1>Kapadyak</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus delectus non sequi officia fugit! Accusantium voluptatem praesentium voluptatibus, perferendis quos maiores enim aliquid corporis. Quaerat illum excepturi laboriosam fugit quae! Illo consectetur sed provident sapiente incidunt dolore. Explicabo culpa temporibus, dolore impedit harum quisquam necessitatibus libero atque repellendus debitis magni odit quis? Nulla harum consequatur nihil accusantium error. Alias facere dolorum, vero iste ab minus deserunt. Nisi provident vel ad debitis ratione necessitatibus sit. Itaque porro fuga placeat neque assumenda dolorem suscipit sit, accusantium ea, minima totam architecto excepturi sint! Officiis, quidem. Libero, ut! Veritatis, iusto aspernatur. Vero necessitatibus delectus consectetur debitis eligendi id, ab alias veritatis laudantium facilis ex obcaecati adipisci assumenda sunt deleniti praesentium earum, laboriosam tempora omnis tempore laborum eaque veniam. Animi nam perspiciatis alias quos velit eveniet, necessitatibus quod neque fuga eos dolores dicta amet facere reprehenderit nemo cum eaque officiis unde? Dolore, aut ipsam quas illum, doloribus adipisci eveniet architecto fugiat voluptates officiis asperiores doloremque est error nesciunt. Voluptas vel quam voluptates tenetur cupiditate, repudiandae mollitia assumenda blanditiis recusandae obcaecati. Iure inventore dolorem veniam totam veritatis libero, harum soluta est possimus optio vel ullam consequuntur. Obcaecati assumenda consequuntur quidem explicabo pariatur aut esse dolore doloribus ipsa, sit quod, eos nam repudiandae reiciendis architecto hic doloremque magnam illum temporibus! Ea similique, necessitatibus ex doloribus quia nulla iusto delectus fuga magnam odit aliquid perferendis in quod cum nisi at ducimus quisquam. Sit, velit obcaecati! Facere debitis quidem eos in corporis fugiat ipsa aspernatur iste nesciunt ab unde, autem quaerat. Harum odio dolore fugit. Ipsa sunt tenetur, unde minima veritatis consequuntur deleniti iste explicabo perspiciatis sapiente. Blanditiis, provident suscipit animi facere perferendis esse quos quo iusto saepe eius sed porro dolores hic? Atque voluptas saepe facere illo, nobis et maiores quos expedita laudantium reiciendis autem adipisci at, totam optio! Vel illo praesentium maxime. Praesentium autem ratione totam, odio libero nihil et placeat aspernatur nesciunt illo minima reiciendis nemo, laudantium pariatur, dolores ipsa! Ut, temporibus cum aut, facilis nobis itaque assumenda consequuntur mollitia sapiente dolorem quae repudiandae est laborum ipsa expedita eius! Exercitationem delectus adipisci officia nemo voluptatum aliquid quos minima modi! Ut facilis voluptatum, nostrum ipsa deserunt quam perspiciatis dolore quo aut dolor sed voluptas amet eaque dolores dolorem ipsam vero cupiditate laudantium officia ullam doloremque molestiae. Cumque voluptate explicabo, necessitatibus, voluptas deserunt, voluptatibus aspernatur neque distinctio possimus sint ratione cupiditate. Expedita eum quaerat similique quod temporibus sunt. Expedita beatae rem iusto voluptatum modi dolores quae, a accusamus dolorem? Pariatur saepe beatae fugiat dolores atque accusamus eos! Perferendis, quaerat explicabo. Eum doloremque error culpa explicabo reprehenderit quisquam fugit itaque, ea laudantium facere consectetur! Quam hic illo laborum consequatur obcaecati impedit, sunt, ducimus iusto quidem cum vitae error ut provident! Placeat, repellendus pariatur aut omnis quos, iste molestiae rem sequi architecto ipsam amet? Odit labore assumenda accusantium, voluptas corporis atque, voluptates tempore iure et sapiente aliquid exercitationem cum officiis eaque incidunt fuga eos culpa dolor! Dignissimos atque beatae ut quam numquam eos iusto a temporibus, repellat facilis obcaecati. Aliquam libero ullam provident tempora voluptas eveniet modi, soluta nostrum repellat, odit sapiente explicabo natus cumque qui autem dolores tempore amet. Deleniti corrupti nihil totam officia! Dolorum tempora non earum repellat incidunt necessitatibus voluptatum explicabo sunt iure sapiente modi cumque sed voluptates eum beatae adipisci id voluptate, corporis eaque aliquid nisi sint facere officia! Fuga, molestiae temporibus provident quo quis repellat deleniti eos, dicta sint nostrum a perspiciatis nam sunt earum fugiat ex quaerat? Recusandae pariatur voluptates in est repudiandae veniam praesentium, sapiente sequi? Omnis est officiis illum veritatis commodi quidem optio nisi, consequuntur nemo ea? Mollitia, sint iusto. Exercitationem illo eligendi magni facilis quo mollitia labore excepturi natus tempora? Quia nulla aspernatur error, inventore veniam vero dolorem nostrum tenetur quo, omnis animi eos, aliquam quos suscipit? Eius tempore facilis neque rerum quia quidem natus perspiciatis, ipsa velit laboriosam optio voluptatum eaque quae fuga? Quos rerum earum veniam eaque quod molestias, officiis assumenda, necessitatibus possimus culpa animi. Odit excepturi totam eum dolor nulla saepe rem, maxime voluptas magni obcaecati! Doloremque voluptas hic facilis sequi praesentium odit veniam culpa eveniet impedit, voluptatibus quis explicabo dolorem ullam libero ratione nemo, temporibus, ipsa eius magnam minus? Nostrum, corporis dignissimos, suscipit ratione accusamus culpa repellendus impedit, nisi ipsam magnam optio! Expedita nobis quae voluptatibus aliquam fugit commodi laboriosam! Possimus, deserunt necessitatibus hic neque fugit commodi porro ut id similique ducimus quam delectus aut ratione earum alias in voluptatum dolore assumenda vitae quibusdam totam rem! Dicta vel blanditiis animi sapiente facere distinctio qui, ipsam dolores quo ullam voluptate aut atque? Suscipit fugit aut iusto officiis rerum earum magni sint soluta eveniet laborum? Minus dolore mollitia, cum nesciunt temporibus optio rerum laboriosam perspiciatis tempora dolorum, esse sapiente reiciendis? Nobis, ex. Asperiores saepe minima voluptate similique aliquam magni non aut ullam enim nesciunt fuga voluptates, expedita hic cupiditate eligendi! Quidem exercitationem labore at dicta, nihil dolores quod quam fugiat facere, error reiciendis magnam unde laborum voluptates temporibus quo ducimus. Hic, vero corporis. In ad inventore labore eum ducimus similique, quia, nisi iste recusandae repudiandae minus harum quaerat quos amet sit quidem iusto dolorem dolores. Alias at voluptatum consequuntur vitae neque error nulla officiis optio quis, illum, asperiores provident dolor vero ab a accusantium voluptates fuga aliquam iure voluptas? Nulla nesciunt libero, inventore, pariatur eligendi nostrum magni nisi corrupti dignissimos voluptas qui maxime! Est labore quibusdam recusandae ratione, nulla nemo, nisi vitae excepturi consequatur temporibus sint? Id sequi ab aliquam dignissimos laboriosam magni ex amet, odit non harum temporibus ad, modi veritatis nihil! Deleniti doloribus eos cumque excepturi vitae sapiente, ex velit iste corrupti hic quia officiis error quos rerum a explicabo. Facilis facere adipisci dolorum deserunt nostrum cumque optio pariatur consequatur, velit, ea at quod itaque atque obcaecati! Vitae quaerat officia quos consectetur in voluptates nobis, eius quod harum magni laborum explicabo voluptas, doloremque animi ullam, totam corrupti nostrum minus natus laudantium? Necessitatibus maxime ea dolores vel placeat alias delectus ullam, quasi eius, itaque quia inventore impedit sapiente. Consequuntur non necessitatibus dolorem harum nostrum mollitia voluptas deserunt laudantium!</p>
+        </main>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php 
-require 'connect/connection.php';
-session_start();
-// Check whether user is logged on or not
-if (isset($_SESSION['user_id'])) {
-    header("location:index.php");
-}
-$temp = $_SESSION['user_id'];
-session_destroy();
-session_start();
-$_SESSION['user_id'] = $temp;
-ob_start(); 
-// Establish Database Connection
+// require 'connect/connection.php';
+// session_start();
+// // Check whether user is logged on or not
+// if (isset($_SESSION['user_id'])) {
+//     header("location:index.php");
+// }
+// $temp = $_SESSION['user_id'];
+// session_destroy();
+// session_start();
+// $_SESSION['user_id'] = $temp;
+// ob_start(); 
+// // Establish Database Connection
 
-?>
+// ?>
 
 <?php
     include('connect/connection.php'); ?>
@@ -24,147 +24,13 @@ ob_start();
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Home / Kapadyak</title>
+	<link rel="ICON" type="image/x-icon" href="Images/logo.ico">
 	<link rel="stylesheet" type="text/css" href="resources/css/main.css">
+	<title>Home / Kapadyak</title>
 </head>
 <body>
     <div class="container">
-	<?php include 'Includes/Header.php'; ?>
-        <br>
-        <div class="createpost">
-            <form method="post" action="" onsubmit="return validatePost()" enctype="multipart/form-data">
-                <h2>Make Post</h2>
-                <hr>
-                <span style="float:right; color:black">
-                <input type="checkbox" id="public" name="public">
-                <label for="public">Public</label>
-                </span>
-                Caption <span class="required" style="display:none;"> *You can't Leave the Caption Empty.</span><br>
-                <textarea rows="6" name="caption"></textarea>
-                <center><img src="" id="preview" style="max-width:580px; display:none;"></center>
-                <div class="createpostbuttons">
-                    <!--<form action="" method="post" enctype="multipart/form-data" id="imageform">-->
-                    <label>
-                        <img src="images/photo.png">
-                        <input type="file" name="fileUpload" id="imagefile">
-                        <!--<input type="submit" style="display:none;">-->
-                    </label>
-                    <input type="submit" value="Post" name="post">
-                    <!--</form>-->
-                </div>
-            </form>
-        </div>
-        <h1>News Feed</h1>
-		<?php 
-        // Public Posts Union Friends' Private Posts
-		
-		$sql = "SELECT newsfeed.post_caption, newsfeed.post_time, newsfeed.post_public, useraccount.first_name,
-		useraccount.last_name, useraccount.user_ID, useraccount.sex, newsfeed.post_id
-FROM newsfeed
-JOIN useraccount
-ON newsfeed.post_by = useraccount.user_ID
-WHERE newsfeed.post_public = 'Y' OR useraccount.user_ID = '{$_SESSION['user_id']}'
-
-UNION
-SELECT newsfeed.post_caption, newsfeed.post_time, newsfeed.post_public, useraccount.first_name,
-useraccount.last_name, useraccount.user_ID, useraccount.sex, newsfeed.post_id
-FROM newsfeed
-JOIN useraccount
-ON newsfeed.post_by = useraccount.user_ID
-JOIN (
-	SELECT friends.user_ID1 AS user_ID
-	FROM friends
-	WHERE friends.user_ID2 = '{$_SESSION['user_id']}' AND friends.friends_status = 1
-	UNION
-	SELECT friends.user_ID2 AS user_ID
-	FROM friends
-	WHERE friends.user_ID1 = '{$_SESSION['user_id']}' AND friends.friends_status = 1
-) useraccountfriends
-ON useraccountfriends.user_ID = newsfeed.post_by
-WHERE newsfeed.post_public = 'N'
-ORDER BY post_time DESC";
-
-        $query = mysqli_query($connect, $sql );
-        if(!$query){
-            echo mysqli_error($connect);
-        }
-        if(mysqli_num_rows($query) == 0){
-            echo '<div class="post">';
-            echo 'There are no posts yet to show.';
-            echo '</div>';
-        }
-        else{
-            $width = '40px'; // Profile Image Dimensions
-            $height = '40px';
-            while($rowCount = mysqli_fetch_assoc($query)){
-                include 'post.php';
-                echo '<br>';
-            }
-			
-        }
-        ?>   <br><br><br>
-		</div>
-		<script src="resources/js/jquery.js"></script>
-		<script>
-			// Invoke preview when an image file is choosen.
-			$(document).ready(function(){
-				$('#imagefile').change(function(){
-					preview(this);
-				});
-			});
-			// Preview function
-			function preview(input){
-				if (input.files && input.files[0]) {
-					var reader = new FileReader();
-					reader.onload = function (event){
-						$('#preview').attr('src', event.target.result);
-						$('#preview').css('display', 'initial');
-					}
-					reader.readAsDataURL(input.files[0]);
-				}
-			}
-			// Form Validation
-			function validatePost(){
-				var required = document.getElementsByClassName("required");
-				var caption = document.getElementsByTagName("textarea")[0].value;
-				required[0].style.display = "none";
-				if(caption == ""){
-					required[0].style.display = "initial";
-					return false;
-				}
-				return true;
-			}
-		</script>
-	</body>
-	</html>
-	
-	<?php
-	if($_SERVER['REQUEST_METHOD'] == 'POST') { // Form is Posted
-		// Assign Variables
-		$caption = $_POST['caption'];
-		if(isset($_POST['public'])) {
-			$public = "Y";
-		} else {
-			$public = "N";
-		}
-		$poster = $_SESSION['user_id'];
-		// Apply Insertion Query
-		$sql = "INSERT INTO newsfeed (post_caption, post_public, post_time, post_by) 
-		VALUES ('$caption', '$public', NOW(),'{$_SESSION['user_id']}')";
-		$query = mysqli_query($connect, $sql);
-		// Action on Successful Query
-		if($query){
-			// Upload Post Image If a file was choosen
-			if (!empty($_FILES['fileUpload']['name'])) {
-				echo 'FUUUQ';
-				// Retrieve Post ID
-				$last_id = mysqli_insert_id($connnect);
-				include 'functions/upload.php';
-			}
-			header("location: home.php");
-		}
-	}
-	?>
-	
+	<?php include 'Includes/Sidebar.php'; ?>
+     
 </body>
 </html>
